40f3b5c7b2e9e3b89afc00058bd4a7b073f3829e
Enzo Roselli <eroselli@emmsa.net>
2020-06-24 18:19:54 -0300

    first commit

---
+++ src/main/java/com/example/ParcialRestIntegration/ParcialRestIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.ParcialRestIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialRestIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialRestIntegrationApplication.class, args);
+	}
+
+}
